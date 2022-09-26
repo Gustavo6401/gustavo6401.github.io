865a8dfd81ff1831e2e667a0eede8593acde14f5
Gustavo6401 <gsoey2019123@gmail.com>
2022-09-25 22:48:22 -0300

    Reorganização dos Arquivos

--- js/setup.js
+++ js/setup.js
@@ -1,27 +1,15 @@
 var informacoes;
 
-function getJson() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', file);
-    xhr.overrideMimeType = 'application/json';
+fetch("json/Informacoes.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
 
-    xhr.onreadystatechange = function () {
-        var status = xhr.status;
-
-        if (status == 200) {
-            informacoes = JSON.parse('json/Informacoes.json');
-
-            var info = document.getElementById('#informacoes');
-
-            info.onload() = function () {
-                info.insertAdjacentHTML('afterbegin',
-                    `<p>E-Mail: <span>${informacoes.email}</span></p><br>` +
-                    `<p>Telefone: <span>${informacoes.telefone}</span></p><br>` +
-                    `<p>Data de Nascimento: <span>${informacoes.data - nascimento}</span></p><br>` +
-                    `<p>Github<a href='${informacoes.github}'>${informacoes.github}</a></p><br>` +
-                    `<p>Linkedin<a href='${informacoes.linkedin}'>${informacoes.linkedin}</a></p>`);
-            }
-        }
-    }
-    xhr.send(null);               
-}
+        document.querySelector('#informacoes')
+            .insertAdjacentHTML('afterbegin',
+                `<p>E-Mail: <span>${data.email}</span></p><br>` +
+                `<p>Telefone: <span>${data.telefone}</span></p><br>` +
+                `<p>Data de Nascimento: <span>${data.dataNascimento}</span></p><br>` +
+                `<p>Github: <a href='${data.github}'>${data.github}</a></p><br>` +
+                `<p>Linkedin: <a href='${data.linkedin}'>${data.linkedin}</a></p>`);
+    });
